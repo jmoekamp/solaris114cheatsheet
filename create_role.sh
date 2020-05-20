fcfcdafdb9852a98266bdf985af218c0889e7961
Joerg Moellenkamp <joergmoellenkamp@MacBook-Pro.(none)>
2020-05-20 20:28:25 +0200

    New role creator script Added LocalesTimezoneKeyboard_SetConsoleKeymap Added LocalesTimezoneKeyboard_SetLocale Added LocalesTimezoneKeyboard_SetTimezone Added LocalesTimezoneKeyboard_ListAvailableConsoleKeymaps Added LocalesTimezoneKeyboard_ListAvailableLocales

--- create_role.sh
+++ create_role.sh
@@ -1,11 +1,24 @@
 #!/bin/zsh
 
-echo $1
+sectionname="$1" 
+functionname="$2" 
+commandstring="$3"
+rolename=$sectionname\_$functionname
+tasksmainymlpath=/Users/joergmoellenkamp/solaris114cheatsheet/roles/$rolename/tasks/main.yml
 
-mkdir -p roles/$1
-cd roles/$1
+mkdir -p roles/$rolename
+cd roles/$rolename
 mkdir defaults files handlers meta templates tasks vars
 cd tasks
-touch main.yml
+pwd
+cp /Users/joergmoellenkamp/solaris114cheatsheet/roletemplate/main.yml $tasksmainymlpath
 
+perl -i -pe s/%%Command%%/$commandstring/g $tasksmainymlpath
+perl -i -pe s/%%Sectionname%%/$sectionname/g $tasksmainymlpath
+perl -i -pe s/%%Functionname%%/$functionname/g $tasksmainymlpath
+
+echo "    - role: $rolename " >> /Users/joergmoellenkamp/solaris114cheatsheet/test.yml
+
+code /Users/joergmoellenkamp/solaris114cheatsheet/test.yml
+code $tasksmainymlpath
 
